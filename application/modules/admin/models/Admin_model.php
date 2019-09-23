d89b0d690a0edb07e0581e2f686b5260ea560094
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2019-09-23 13:24:41 -0600

    Ajuste de bug con el modelo para consutar usuarios

--- application/modules/admin/models/Admin_model.php
+++ application/modules/admin/models/Admin_model.php
@@ -16,6 +16,31 @@
 					return true;
 				} else{ return false; }
 		}
+		
+        /**
+         * Lista de usuarios
+         * @since 10/5/2017
+         */
+        public function get_users($arrDatos) 
+        {
+                $this->db->select();
+                $this->db->join('param_roles R', 'R.id_rol = U.fk_id_rol', 'INNER');
+                if (array_key_exists("idUsuario", $arrDatos)) {
+                    $this->db->where('id_usuario', $arrDatos["idUsuario"]);
+                }
+                if (array_key_exists("idRol", $arrDatos)) {
+                    $this->db->where('fk_id_rol', $arrDatos["idRol"]);
+                }
+                
+                $this->db->order_by('nombres_usuario', 'asc');
+                $query = $this->db->get('usuario U');
+
+                if ($query->num_rows() > 0) {
+                    return $query->result_array();
+                } else {
+                    return false;
+                }
+        }
 				
 		/**
 		 * Add/Edit USER
